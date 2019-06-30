a2ed8c0611585f1f56af6b4e6c838f854290a5e5
Leo <leo.francocrespo@gmail.com>
2019-06-30 14:49:56 -0500

    converted url links to use flask's url_for function

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 
 from app import app
 from app.forms import LoginForm 
@@ -27,6 +27,6 @@ def login():
         username = form.username.data 
         remember_me = form.remember_me.data 
         flash(f'Login requested for user {username}, remember_me={remebmer_me}') 
-        return redirect('/index')
+        return redirect(url_for('index'))
     
     return render_template('login.html', title='Sign In', form=form)
